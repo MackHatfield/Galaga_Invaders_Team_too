98d895e688ae42429e40f376a0eb5fb42bda7bf5
Steve Finch <finchst@oregonstate.edu>
2019-02-04 11:30:47 -0800

    add position to ship

--- main.rb
+++ main.rb
@@ -2,15 +2,16 @@ class GalagaInvaders
 
   def initialize
     @caption = "Galaga Invaders"
+    @player_ship = Ship.New(x, y)
   end
 
   def update
-    # ...
+    @player_ship.move
   end
 
   def draw
-    # ...
-  end
+    @player_ship.draw
+    end
 
   def show
     while (true) do
