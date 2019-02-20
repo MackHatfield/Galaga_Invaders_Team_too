56958382c632db5bb03bfd951b786715c167e374
Mackenzie <therealbakoonzar@gmail.com>
2019-02-20 10:59:45 -0800

    update ship to the form of Yong's gist. References #12

--- ship.rb
+++ ship.rb
@@ -1,17 +1,55 @@
 class Ship
 
-  def initialize(x, y)
-    @x = x
-    @y = y
+  WIDTH = 100
+  HEIGHT = 100
+  DEFAULT_VELOCITY = 5
+  ORDNANCE_VELOCITY = -10
+
+  attr_accessor :x, :y
+
+  def initialize(screen_width, screen_height)
+    @x = screen_width / 2
+    @y = screen_height - half_height
   end
 
-  def move()
+  def ordnance_velocity
+    ORDNANCE_VELOCITY
   end
 
-  def shoot()
+  def muzzle_location
+    Vector.new(x, top_edge)
   end
 
-  def die()
+  def move_left
+    move(-DEFAULT_VELOCITY)
   end
-    
+
+  def move_right
+    move(DEFAULT_VELOCITY)
+  end
+
+  def draw
+    puts self
+  end
+
+  def to_s
+    "✈️ (#{x}, #{y})"
+  end
+
+  private
+
+  def move(delta)
+    self.x += delta
+  end
+
+  def top_edge
+    y - half_height
+  end
+
+  def half_height
+    HEIGHT / 2
+  end
+
 end
+
+Vector = Struct.new(:x, :y)
